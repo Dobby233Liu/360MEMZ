193e936fd257fc7d6f03513ec696aa9c5633fee4
Dobby233Liu <papaqi1234567@126.com>
2019-01-29 11:54:40 +0800

    no

--- WindowsTrojan/Build/Data.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../src/memz.h"
-#define code1Len 145
-#define code2Len 2686
-extern const unsigned char code1[];
-extern const unsigned char code2[];
-extern const size_t code1_len;
-extern const size_t code2_len;
